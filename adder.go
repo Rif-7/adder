e685c4740a80fb45c7cfaa93f9e433c374c732ad
Rifan <muhammadrifan974530@gmail.com>
2025-07-22 22:15:17 +0530

    v1.0.1: add GoDoc comments

--- adder.go
+++ adder.go
@@ -1,5 +1,10 @@
+// Package adder provides a simple example of an addition function.
+//
+// For a basic introduction to addition, see:
+// https://www.mathsisfun.com/numbers/addition.html
 package adder
 
+// Add returns the sum of two integers.
 func Add(x, y int) int {
 	return x + y
 }
